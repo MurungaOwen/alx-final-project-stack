7e1c510cab945f835b4e1f32c9840cbd7988bf72
MurungaOwen <owenhood80@gmail.com>
2024-05-21 14:41:47 +0300

    register functionality implemented

--- backend/controllers/UsersControllers.js
+++ backend/controllers/UsersControllers.js
@@ -1,28 +1,41 @@
 const dbClient = require('../utils/db');
 const crypto = require('crypto');
 
+function encryptPassword(password) {
+    const sha1 = crypto.createHash('sha1');
+    sha1.update(password);
+    const hashedPassword = sha1.digest('hex');
+    return hashedPassword;
+}
+
 export async function RegisterUser(req, res){
     // TODO: find if user already exists and if not add them to db
-    const { email, password, role } = req.body;
-    if(!email) {
-        return res.status(400).send({"error": "Enter Email"});
+    const { firstname, lastname, phonenumber, password, role} = req.body;
+    if(!firstname || !lastname) {
+        return res.status(400).send({"error": "Enter both names"});
     }
     if(!role) {
         return res.status(400).send({"error": "Add the role e.g Tenant, Owner, Painter"});
     }
+    if(!phonenumber) {
+        return res.status(400).send({"error": "Enter phone number"});
+    }
     if(!password) {
-        return res.status(400).send({"error": "ENter password"});
+        return res.status(400).send({"error": "Enter password"});
     }
+    try {
+        const exists = await dbClient.getUserWithPhone(phonenumber);
 
-    const user = await dbClient.getUserWithEmail(email);
+        if (!exists) {
+            const hashedPassword = encryptPassword(password)
+            const newUser = await dbClient.createUser(firstname, lastname, phonenumber, hashedPassword, role);
+            console.log(`new User: ${newUser.ops}`)
+            return res.status(201).send({"email": email, "id": newUser.ops._id});
+        }
+        // user exists
+        return res.status(400).send({"error": "User with the phone number is already registerd"});
 
-    if (!user) {
-        const hash = crypto.createHash('sha256');
-        const hashedPassword = hash.update(password);
-        const user = await dbClient.createUser(email, hashedPassword, role);
-        return res.status(201).send({"email": user.email, "id": user._id});
+    } catch (error) {
+        return res.status(500).send({"error": "can\'t process your request at the moment"});
     }
-    // user exists
-    return res.status(400).send({"error": "User with the email already exists"});
-
 }
