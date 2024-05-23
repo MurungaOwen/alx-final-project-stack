027ecdc48a0cfb56e4409876dadc3dd058f095e4
MurungaOwen <owenhood80@gmail.com>
2024-05-24 02:28:24 +0300

    restructure timer

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -1,11 +1,13 @@
 import axios from "axios";
-import { getTimestamp } from "../utils/timestamp";
 import  'dotenv/config';
 
+// TODO: look at why env variables are not being accessed;
 export async function generateToken (req, res, next ) {
+
+  // generate token for identification at safaricom
   const CONSUMER_KEY = process.env.MPESA_CONSUMER_KEY;
   const CONSUMER_SECRET = process.env.MPESA_CONSUMER_SECRET;
-  console.log("consumer key", CONSUMER_KEY)
+
   const URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
 
   const auth = Buffer.from(`${CONSUMER_KEY}:${CONSUMER_SECRET}`).toString(
@@ -18,7 +20,7 @@ export async function generateToken (req, res, next ) {
         Authorization: `Basic ${auth}`,
       },
     });
-    req.token = response.data.access_token;
+    req.token = response.data.access_token; // get token
     next(); // execute next function
 
   } catch (error) {
@@ -35,6 +37,7 @@ export async function handleStkPush (req, res) {
     const password = Buffer.from(
       BUSINESS_SHORT_CODE + process.env.MPESA_PASS_KEY + getTimestamp()
     ).toString("base64");
+    // password created by making business till number and time of transaction and our pass key
   
     const payload = {
       BusinessShortCode: BUSINESS_SHORT_CODE,
@@ -42,17 +45,18 @@ export async function handleStkPush (req, res) {
       Timestamp: getTimestamp(),
       TransactionType: "CustomerPayBillOnline",
       Amount: amount,
-      PartyA: phone,
-      PartyB: 174379,
-      PhoneNumber: phone,
-      CallBackURL: "https://5bb5-41-90-66-254.ngrok-free.app",
+      PartyA: phone, // tenant number
+      PartyB: 174379, // owners number but we use this to test
+      PhoneNumber: phone, // tenants nnumber still
+      CallBackURL: "", // where to send client after success 
       AccountReference: "Rafiki rent payment",
       TransactionDesc: "Rafiki",
     };
   
     try {
+      const stkpush = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
       const response = await axios.post(
-        "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
+        stkpush,
         payload,
         {
           headers: {
@@ -60,15 +64,26 @@ export async function handleStkPush (req, res) {
           },
         }
       );
-      res.status(201).json({
-        message: true,
-        data: response.data,
-      });
+      res.status(201).json({ message: true, data: response.data });
+
     } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        message: "Failed",
-        error: error.message,
-      });
+      res.status(500).json({ message: "Failed", error: error.message});
     }
   };
+
+
+function parseDate(val) {
+  return (val < 10) ? "0" + val : val;
+}
+
+const getTimestamp = ()=>{
+
+    const dateString  = new Date().toLocaleString("en-us", {timeZone: "Africa/Nairobi"});
+    const dateObject = new Date(dateString);
+    const month  = parseDate(dateObject.getMonth() + 1);
+    const day  = parseDate(dateObject.getDate());
+    const hour = parseDate(dateObject.getHours());
+    const minute = parseDate(dateObject.getMinutes());
+    const second = parseDate(dateObject.getSeconds());
+    return dateObject.getFullYear() + "" + month + "" + day + "" + hour + "" + minute + "" + second;
+}
