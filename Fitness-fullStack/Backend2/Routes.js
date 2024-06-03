6cc819d2db92bd882eee9783a39ada281bf0c0bc
emmanuel tindi <emmanueltindi66@gmail.com>
2024-06-03 16:46:06 +0300

    update gifurls

--- Fitness-fullStack/Backend2/Routes.js
+++ Fitness-fullStack/Backend2/Routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { RegisterUser, Login } from './Controllers/usersConroller.js';
-import { getExercise } from './Controllers/excercisesController.js';
+import { getExercise, createFitnessProgram, getFitnessProgram } from './Controllers/excercisesController.js';
 
 const router = express.Router();
 
@@ -8,8 +8,8 @@ router.post('/register', RegisterUser);
 router.post('/login', Login);
 router.get('/categories/bodypart');
 router.get('/exercise/:body_part', getExercise);
-router.post('/fitness_program');
-router.get('/fitness_programs');
+router.post('/fitness_program', createFitnessProgram);
+router.get('/fitness_program/:userId', getFitnessProgram);
 router.put('/fitness_program/:program_id');
 router.delete('/fitness_program/:program_id');
 router.get('/fitness_programs/count');
