1829c0c52abd06d855e784ac570571ddf7264ad4
MurungaOwen <owenhood80@gmail.com>
2024-06-04 23:35:59 +0300

    use bcrypt for password

--- backend/utils/encrypto.js
+++ backend/utils/encrypto.js
@@ -1,20 +1,15 @@
 import crypto from 'crypto';
-
-// Function to generate a salt
-export const generateSalt = (length = 16) => {
-    return crypto.randomBytes(length).toString('hex');
-};
-
+import bcrypt from "bcrypt";
 // Function to hash the password with the salt
-export const encryptPassword = (password, salt) => {
-    const hash = crypto.createHash('sha1');
-    hash.update(password + salt);
-    return hash.digest('hex');
+
+const SALT_ROUNDS = 10;
+
+export const encryptPassword = async (password) => {
+    const salt = await bcrypt.genSalt(SALT_ROUNDS);
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
 };
 
-export function verifyPassword(password, salt, storedHash) {
-    const hashedPassword = encryptPassword(password, salt);
-    console.log(`hash passwd entered: ${hashedPassword}`);
-    console.log(`hash passwd in db: ${storedHash}`);
-    return hashedPassword === storedHash;
-}
+export const comparePassword = async (password, hash) => {
+    return await bcrypt.compare(password, hash);
+};
